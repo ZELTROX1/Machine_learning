83d58a63741d105ccf533c62cc21ed5e382c874c
[praneeth bhavana] <[santhibhavana1234@gmail.com]>
2024-08-28 00:00:11 +0530

    Adding projects

--- support vector machine (SVM)/SVM.py
+++ support vector machine (SVM)/SVM.py
@@ -48,7 +48,7 @@ X = X[y != 2]
 y = y[y != 2]
 
 # Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 # Standardize the features
 scaler = StandardScaler()
